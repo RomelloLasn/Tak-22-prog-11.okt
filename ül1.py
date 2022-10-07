51147f2fe3f78d85987622a9ae07ba8178dc0efa
RomelloLasn <romello.lasn@ametikool.ee>
2022-10-07 11:38:48 +0300

    7.okt rounding

---
+++ ül1.py
@@ -0,0 +1,11 @@
+user_name = ('kroonid eurodeks' + '?')
+print('hello,' 'kroonid eurodeks')
+
+kroonid = ('kroonid')
+eurod = ('eurod')
+
+a = int(input('kroonid: '))
+b = int(input('eurod: '))
+
+print(a/b)
+
