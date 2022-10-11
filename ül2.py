d3ddb18156be6386068076bebced940223c42512
RomelloLasn <romello.lasn@ametikool.ee>
2022-10-11 13:29:22 +0300

    ül 2 11.okt

--- ül2.py
+++ ül2.py
@@ -3,3 +3,15 @@ import math
 
 #print the value of pi
 print(math.pi)
+
+class Circle:
+    def __init__(self, r):
+        self.radius = r
+    def area(self):
+        return 3.14 * (self.radius ** 2)
+    def perimeter(self):
+        return 2*3.14*self.radius
+obj = Circle(int(input("Enter Radius:")))
+#obj = Circle()
+print("Area of circle:",obj.area())
+print("Perimeter of circle:",obj.perimeter())
