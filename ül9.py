5b6a98d784660cafcec7ccbb12576eacc4aab9ea
Romello Lasn <romello.lasn@ametikool.ee>
2022-10-26 11:23:25 +0300

    Tak22-prog

---
+++ ül9.py
@@ -0,0 +1,13 @@
+a = float(input("esimene külg: "))
+b = float(input("teine külg: "))
+c = float(input("alus: "))
+
+if a == b == c:
+    print("võrdkülgne")
+
+elif  print(a <= 0) or (b <= 0) or (a <= 0):
+    print("võimatu")
+elif a == b:
+    print("võrdhaarne")
+else:
+    print("erikülgne")    
