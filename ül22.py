ab8dacc95429038022f48a9413ec2831d9ee9b12
RomelloLasn <112690676+RomelloLasn@users.noreply.github.com>
2023-05-04 10:37:09 +0300

    Update ül22.py

--- ül22.py
+++ ül22.py
@@ -1,25 +1,28 @@
 import random
-choice = ['kivi', 'paber', 'käärid']
 
-while true: 
+while True:
+    valikud = ["kivi", "paper", "käärid"]
+    arvuti = random.choice(valikud)
+    mina = input("Sisesta oma valik (kivi, paper, käärid): ")
+
+    if mina == arvuti:
+        print("Viik!")
+    elif mina == "kivi":
+        if arvuti == "käärid":
+            print("Sa võitsid!")
+        else:
+            print("Sa kaotasid!")
+    elif mina == "paper":
+        if arvuti == "kivi":
+            print("Sa võitsid!")
+        else:
+            print("Sa kaotasid!")
+    elif mina == "käärid":
+        if arvuti == "paper":
+            print("Sa võitsid!")
+        else:
+            print("Sa kaotasid!")
     
-print('0) ei soovi rohkem mängida')
-print('1)kivi')
-print('2)paber')
-print('3)käärid')
-user_choice = int(input('Tee valik '))
-computer_choice = random.randint(1, 3)
-
-if user_choice != 0:
-    break
-
-print('sinu valik oli' , choices[user_choice-1])
-print('arvuti valik oli' , choices[computer_choice-1])
-
-
-if user_choice == computer_choice:
-    print('viik!')
-elif user_choice == 1 and computer_choice == 3 or user_choice == 2 and computer_choice == 1 or user_choice == 3 and computer_choice == 2:
-    print('sinu võit!')
-else:
-    print('arvuti võit')
+    uuesti = input("Mängime uuesti? (y/n): ")
+    if uuesti == "n":
+        break
